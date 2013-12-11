0d6fe09f27a7d94b3861735e455241122d351d20
zhangzhi <z2care@gmail.com>
2013-12-11 23:50:00 +0800

    merge code into single activity.

--- src/com/drrtest/GLSurfaceViewStubActivity.java
+++ src/com/drrtest/GLSurfaceViewStubActivity.java
@@ -17,8 +17,12 @@
 package com.drrtest;
 
 import android.app.Activity;
+import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -28,11 +32,66 @@ import javax.microedition.khronos.opengles.GL10;
  * Also accepts non-blank renderers to allow its use for more complex tests.
  */
 public class GLSurfaceViewStubActivity extends Activity {
+    private static final float FPS_TOLERANCE = 2.0f;
 
+    private static final String TAG = "GLSurfaceViewStubActivity";
+    
     private static class Renderer implements GLSurfaceView.Renderer {
 
+        // Measurement knobs.
+        // NB: Some devices need a surprisingly long warmup period before the
+        // framerate becomes stable.
+        private static final float WARMUP_SECONDS = 2.0f;
+        private static final float TEST_SECONDS   = 8.0f;
+    	
+        // Test states
+        private static final int STATE_START  = 0;
+        private static final int STATE_WARMUP = 1;
+        private static final int STATE_TEST   = 2;
+        private static final int STATE_DONE   = 3;
+        
+        private int       mState     = STATE_START;
+        private float     mStartTime = 0.0f;
+        private int       mNumFrames = 0;
+    	
         public void onDrawFrame(GL10 gl) {
-            // Do nothing.
+            float t = (float)System.nanoTime() * 1.0e-9f;
+            switch (mState) {
+                case STATE_START:
+                    mStartTime = t;
+                    mState = STATE_WARMUP;
+                    break;
+
+                case STATE_WARMUP:
+                    if ((t - mStartTime) >= WARMUP_SECONDS) {
+                        mStartTime = t;
+                        mNumFrames = 0;
+                        mState = STATE_TEST;
+                    }
+                    break;
+
+                case STATE_TEST:
+                    mNumFrames++;
+                    float elapsed = t - mStartTime;
+                    if (elapsed >= TEST_SECONDS) {
+                        //mResult.notifyResult((float)mNumFrames / elapsed);//handle msg
+                        mState = STATE_DONE;
+                    }
+                    break;
+
+                case STATE_DONE:
+                    //if (mResult.restartNecessary()) {
+                        //mResult.ackRestart();
+                        mState = STATE_START;
+                        Log.d(TAG, "restarting");
+                    //}
+                    break;
+            }
+
+            // prevent unwanted optimizations or hidden costs (e.g. reading
+            // previous frame on tilers).
+            gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            gl.glClear(gl.GL_COLOR_BUFFER_BIT);
         }
 
         public void onSurfaceChanged(GL10 gl, int width, int height) {
@@ -101,8 +160,42 @@ public class GLSurfaceViewStubActivity extends Activity {
             mView.setRenderMode(mRenderMode);
         }
         setContentView(mView);
+        
+        GLSurfaceViewStubActivity.setRenderer(new Renderer());
+        GLSurfaceViewStubActivity.setRenderMode(
+                GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+        
+        
     }
 
+    public void testRefreshRate() throws java.lang.InterruptedException {
+        boolean fpsOk = false;
+        //GLSurfaceViewStubActivity activity = getActivity();
+
+        WindowManager wm = (WindowManager)this
+                .getView()
+                .getContext()
+                .getSystemService(Context.WINDOW_SERVICE);
+        Display dpy = wm.getDefaultDisplay();
+        float claimedFps = dpy.getRefreshRate();
+
+        //for (int i = 0; i < 3; i++) {
+            //float achievedFps = mResult.waitResult();
+            //Log.d(TAG, "claimed " + claimedFps + " fps, " + "achieved " + achievedFps + " fps");
+            //fpsOk = Math.abs(claimedFps - achievedFps) <= FPS_TOLERANCE;
+            //if (fpsOk) {
+                //break;
+            //} else {
+                // it could be other sctivity like bug report capturing for other failures
+                // sleep for a while and re-try
+                //Thread.sleep(10000);
+                //mResult.restart();
+            //}
+        //}
+        //activity.finish();
+        //assertTrue(fpsOk);
+    }
+    
     public GLSurfaceView getView() {
         return mView;
     }
